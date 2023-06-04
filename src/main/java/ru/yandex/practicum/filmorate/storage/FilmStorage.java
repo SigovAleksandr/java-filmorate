c9fd91f3087faab79abde7d5494af7f770c78ab4
Aleksandr Sigov <paperrcutt@yandex.ru>
2023-06-04 21:20:44 +0300

    codestyle fix

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -13,6 +13,8 @@ public interface FilmStorage {
     Film addFilm(Film film);
 
     Film updateFilm(Film film);
+
     void addLike(int filmId, int userId);
+
     void deleteLike(int filmId, int userId);
 }
